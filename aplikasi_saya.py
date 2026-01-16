7d2a347b47d6dc48a17ca49e6c2a37d39ddee392
irgifarhan <irgifarhan@irgi.com>
2026-01-16 08:02:34 +0000

    Luas Segitiga

--- aplikasi_saya.py
+++ aplikasi_saya.py
@@ -1,2 +1,17 @@
-print("Halo! Ini adalah kode yang dibuat di Ubuntu.")
-print("Nanti akan dipush ke GitHub.")
+# Program Menghitung Luas Segitiga
+print("PROGRAM HITUNG LUAS SEGITIGA")
+print("="*30)
+
+
+# Input alas dan tinggi
+alas = float(input("Masukkan alas segitiga: "))
+tinggi = float(input("Masukkan tinggi segitiga: "))
+
+
+# Proses perhitungan
+luas = 0.5 * alas * tinggi
+
+
+# Output
+print("Luas segitiga adalah:", luas)
+print("Program dijalankan di Ubuntu Server via Git")
